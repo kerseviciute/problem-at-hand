82a40735eeaae81d2b614d08e92d94cbfbdd56da
Ieva <kerseviciute.ieva@gmail.com>
2023-11-21 14:26:15 +0200

    Bug fix.

--- python/montage_to_csv.py
+++ python/montage_to_csv.py
@@ -7,4 +7,4 @@ data = pd.DataFrame(data)
 channels = [ f'X{x}' for x in range(0, 256) ]
 data.insert(0, 'ChannelName', channels)
 
-data.to_csv(snakemake.output[ 'csv' ], index = False, header = False)
+data.to_csv(snakemake.output[ 'csv' ], index = False)
