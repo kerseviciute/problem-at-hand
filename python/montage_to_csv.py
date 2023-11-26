c3dceb9240f81f01c1b8bf58b0a4049c2c777283
Ieva <kerseviciute.ieva@gmail.com>
2023-11-26 16:52:14 +0200

    Reformat files.

--- python/montage_to_csv.py
+++ python/montage_to_csv.py
@@ -1,10 +1,13 @@
 from scipy.io import loadmat
 import pandas as pd
 
-data = loadmat(snakemake.input[ 'mat' ])[ 'pos_256' ]
+with open('.montage_to_csv.py.pkl', 'wb') as file:
+    pickle.dump(snakemake, file)
+
+data = loadmat(snakemake.input['mat'])['pos_256']
 data = pd.DataFrame(data)
 
-channels = [ f'X{x}' for x in range(0, 256) ]
+channels = [f'X{x}' for x in range(0, 256)]
 data.insert(0, 'ChannelName', channels)
 
-data.to_csv(snakemake.output[ 'csv' ], index = False)
+data.to_csv(snakemake.output['csv'], index = False)
--- python/prepare_data.py
+++ python/prepare_data.py
@@ -4,44 +4,47 @@ import pickle
 import pandas as pd
 import numpy as np
 
+with open('.prepare_data.py.pkl', 'wb') as file:
+    pickle.dump(snakemake, file)
+
 
 def extract_event(events, event, sample_rate):
     present_event = events == event
-    change_indices = np.where(np.diff(present_event))[ 0 ]
+    change_indices = np.where(np.diff(present_event))[0]
     change_indices = np.insert(change_indices, 0, -1)
 
     event_data = pd.DataFrame({
-        'EventStart': (change_indices[ :-1 ] + 1) / sample_rate,
-        'EventEnd': (change_indices[ 1: ] + 1) / sample_rate,
-        'Event': events[ change_indices[ :-1 ] + 1 ]
+        'EventStart': (change_indices[:-1] + 1) / sample_rate,
+        'EventEnd': (change_indices[1:] + 1) / sample_rate,
+        'Event': events[change_indices[:-1] + 1]
     })
 
-    event_data = event_data[ event_data[ 'Event' ] == event ]
+    event_data = event_data[event_data['Event'] == event]
 
     return event_data
 
 
-montage = mne.channels.read_custom_montage(snakemake.input[ 'montage' ])
+montage = mne.channels.read_custom_montage(snakemake.input['montage'])
 
-data = loadmat(snakemake.input[ 'raw' ])[ 'y' ]
+data = loadmat(snakemake.input['raw'])['y']
 
-timepoints = data[ 0, : ]
-events = data[ -1, : ]
+timepoints = data[0, :]
+events = data[-1, :]
 
-data = data[ 1:-1, ]
+data = data[1:-1, ]
 
-channels = [ f'X{x}' for x in range(0, data.shape[ 0 ]) ]
+channels = [f'X{x}' for x in range(0, data.shape[0])]
 
-sfreq = snakemake.config[ 'sampleRate' ]
+sfreq = snakemake.config['sampleRate']
 
-ch_types = [ 'eeg' for x in range(0, data.shape[ 0 ]) ]
+ch_types = ['eeg' for x in range(0, data.shape[0])]
 
 info = mne.create_info(ch_names = channels, sfreq = sfreq, ch_types = ch_types)
 
 raw = mne.io.RawArray(data, info)
 raw.set_montage(montage)
 
-with open(snakemake.output[ 'raw' ], 'wb') as file:
+with open(snakemake.output['raw'], 'wb') as file:
     pickle.dump(raw, file)
 
 extracted_events = pd.concat([
@@ -57,5 +60,5 @@ extracted_events = pd.concat([
 extracted_events = extracted_events.sort_values(by = 'EventStart')
 extracted_events = extracted_events.reset_index(drop = True)
 
-with open(snakemake.output[ 'events' ], 'wb') as file:
+with open(snakemake.output['events'], 'wb') as file:
     pickle.dump(extracted_events, file)
